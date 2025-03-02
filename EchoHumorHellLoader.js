81ff649b88529d94b6a404df9ef9ad3f1068c3b9
EmptyEcho-Sys <aaaaaahohuv@gmail.com>
2025-03-01 20:49:58 -0500

    Update EchoHumorHellLoader.js

--- EchoHumorHellLoader.js
+++ EchoHumorHellLoader.js
@@ -1,6 +1,6 @@
 document.addEventListener('corru_entered', ()=>{
     if(page.path == '/local/beneath/embassy/' || page.path == '/local/ozo/') {
-    addResources(["https://github.com/EmptyEcho-Sys/Echo_Memhole/edit/main/EchoHumorusHell.js"])
+    addResources(["https://github.com/EmptyEcho-Sys/Echo_Memhole/main/EchoHumorusHell.js"])
     addResources(["https://adenator.neocities.org/corrumods/literallyTooManyHumors.js"])
     }
 })
